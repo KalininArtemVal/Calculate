bd1893ef0adac1981865d83a3a163a9771b420b2
Artem Kalinin <KalininArtemVal@gmail.com>
2020-06-18 19:51:10 +0300

    done v2

--- Calculate/Course2Week3Task1/Code/CalculatorViewController.swift
+++ Calculate/Course2Week3Task1/Code/CalculatorViewController.swift
@@ -26,18 +26,19 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var calculateOutlet: UIButton!
     
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //настраиваем интерфейс согласно ТЗ
         self.view.backgroundColor = #colorLiteral(red: 0.1764705882, green: 0.1843137255, blue: 0.1921568627, alpha: 1)
         self.resultLeble.backgroundColor = #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1)
-        self.plusMinusOutlet.tintColor = #colorLiteral(red: 0.9254901961, green: 0.4431372549, blue: 0.2862745098, alpha: 1)
         self.plusMinusOutlet.layer.cornerRadius = 5
-        self.sliderOutlet.tintColor = #colorLiteral(red: 0.9254901961, green: 0.4431372549, blue: 0.2862745098, alpha: 1)
+        self.plusMinusOutlet.tintColor = .none
+        self.plusMinusOutlet.layer.borderWidth = 1
+        self.plusMinusOutlet.layer.borderColor = #colorLiteral(red: 0.9254901961, green: 0.4431372549, blue: 0.2862745098, alpha: 1)
+        self.plusMinusOutlet.layer.shadowColor = #colorLiteral(red: 0.9254901961, green: 0.4431372549, blue: 0.2862745098, alpha: 1)
+        //настраиваем цвет кнопки calculate
         self.calculateOutlet.backgroundColor = #colorLiteral(red: 0.9254901961, green: 0.4431372549, blue: 0.2862745098, alpha: 1)
-        
-        
-        
         //настраиваем цвет остального текста
         self.firstOperandLable.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         self.firstIntegerLable.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
@@ -53,6 +54,7 @@ class CalculatorViewController: UIViewController {
         self.sliderOutlet.maximumValue = 100
     }
     
+    
     @IBAction func plusMinusButton(_ sender: Any) {
         firstIntegerLable.text = String(format: "%.4f", self.plusMinusOutlet.value)
     }
@@ -66,12 +68,11 @@ class CalculatorViewController: UIViewController {
         guard let first = Double(firstIntegerLable.text ?? "") else { return }
         guard let second = Double(secondIntegerLable.text ?? "") else { return }
         let result = first * second
-        
         if (result.truncatingRemainder(dividingBy: 1) == 0) {
             self.resultLeble.text = "\(Int(result))"
         } else {
             self.resultLeble.text = String(format: "%.4f", result)
-        }  
+        }
     }
     
     
